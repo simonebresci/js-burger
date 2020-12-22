f8198b9f998b5992f9f27c3fea60ebec5c7d3341
Simone <simone.bresci.05@gmail.com>
2020-12-22 12:07:41 +0100

    aggiunto array coupons

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
 // Dichiarazione variabili
 var burgerName =  document.getElementById('burger-name');
 var btnCalculate = document.getElementById('btnCalculate');
-var listaIngradienti = document.getElementsByClassName('checkboxes-container')[0].getElementsByTagName('input');
+var listaIngradienti = document.getElementsByClassName('checkboxes-container')[0].getElementsByTagName('input');  //Prende anche gli input annidati in altri tag (es. div)
 var finalPrice = document.getElementById('finalPrice');
 var discountCode = document.getElementById('discount-code');
+
+// Inizializza variabili
 var sconto = 0;
 var nCheck = 0;
 var prezzo = 0;
+var coupons = ['stringa1','stringa2','stringa3','stringa4','stringa5'];
 
 
 // Bottone calcola
 btnCalculate.addEventListener('click', function() {
   // Inizializza variabili
-  error=false;
+  error  = false;
   prezzo = 0;
-  nCheck=0;
+  nCheck = 0;
+  sconto = 0;
 
   // Controlla dati immessi
   // Campo nome burger non vuoto
@@ -41,10 +45,23 @@ btnCalculate.addEventListener('click', function() {
     // Applica sconto
     if (!error){
       console.log(discountCode.value);
-      if (discountCode !== ''){
-        if (discountCode.value === 'sconto20'){
-          sconto = 20;
-          alert('Codice valido: applico sconto');
+      if (discountCode.value !== ''){
+        // if (discountCode.value === 'sconto20'){
+        //   sconto = 20;
+        //   alert('Codice valido: applico sconto');
+        // }
+
+        // controlla listaCoupons
+        for (var x = 0; x< coupons.length; x++){
+          if (discountCode.value === coupons[x]){
+            sconto = 20;
+            alert('Codice valido: applico sconto');
+            }
+        }
+
+        // Codice sconto non valido
+        if (sconto === 0){
+          alert('codice sconto non valido.');
         }
 
       }
